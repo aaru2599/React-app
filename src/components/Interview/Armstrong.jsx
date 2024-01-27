236acdb739850144b6311001ae5a82e1920fe022
aaru2599 <arvindmandre2@gmail.com>
2024-01-27 21:01:12 +0530

    Armstrong with react

--- src/components/Interview/Armstrong.jsx
+++ src/components/Interview/Armstrong.jsx
@@ -1,15 +1,33 @@
-import React, { useState } from "react"
+import React, {  useState } from "react"
 const Armstrong = () => {
-    const [userInput,setUserInput]=useState(0)
-    const handleBtnClick=()=>{
-        console.log(userInput);
+    const [userInput, setUserInput] = useState()
+    const [displayRes, setDisplayRes] = useState(false);
+
+    const handleBtnClick = () => {
+        let numArr = userInput.split("");
+        const len = numArr.length;
+        console.log(len);
+        let result = 0;
+        for (let i = 0; i < len; i++) {
+            let sum = numArr[i] ** len
+            result = result + sum
+        }
+        if (result == userInput) {
+            setDisplayRes(true)
+        }else{
+            setDisplayRes(false)
+        }
+        // console.log(numArr);
     }
-    
-    console.log(userInput);
+
+    // console.log(userInput);
     return (
         <div>
-            <input value={userInput} onChange={(e)=>setUserInput(e.target.value)}  type="text" />
+            <input value={userInput} onChange={(e) => setUserInput(e.target.value)} type="text" />
             <button onClick={handleBtnClick}>Submit</button>
+            {
+                displayRes?<p>Armstrong Number</p>:<p>Not An Armstrong Number</p>
+            }
         </div>
     )
 }
